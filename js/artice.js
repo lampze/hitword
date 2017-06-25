7d698931090afb041ba7df4e7921d06dc090f33a
shirui <1229408499@qq.com>
2017-06-25 15:23:08 +0800

    实现了填充文字，和验错的功能

--- js/artice.js
+++ js/artice.js
@@ -1,7 +1,10 @@
-function Artic(main) {
+function Artice(main, title) {
   return {
     main: main,
-    
+    next: main,
+    history: [],
+    title: title,
+    now: "",
     numWords: 0,
     trueNums: 0,
     time: 0,
@@ -15,7 +18,36 @@ function Artic(main) {
       return this.numWords / this.minutes();
     },
     correct: function() {
-      return this.trueNums / this.numWords
+      return this.trueNums / this.numWords * 100
+    },
+    sliceStr: function(obj) {
+      for (var count=0; obj.scrollHeight <= obj.offsetHeight; count++) {
+          obj.innerHTML = obj.innerHTML + this.next[count];
+      }
+      count--;
+      this.now = this.next.slice(0,count);
+      this.next = this.next.slice(count);
+      this.history.push([this.now]);
+    },
+    plan: function(obj) {
+      obj.innerHTML = null;
+      for(var num=0; num<this.now.length; num++) {
+          obj.innerHTML += "<span class=\"change\">"+this.now[num]+"</span>";
+      }
+    },
+    changeColor: function(read, hit) {
+      return function() {
+        for(var num=0;num<hit.value.length+1;num++) {
+          if(hit.value[num] == read[num].innerHTML) {
+            read[num].style.color = "green";
+          }else {
+            read[num].style.color = "red";
+          }
+        }
+        for(var num=hit.value.length;num<read.length;num++) {
+          read[num].style.color = "black";
+        }
+      }
     }
   }
 }
--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 var conBanner = document.getElementsByClassName('con-banner');
-
 function getMousePos(event) {
   var e = event || window.event;
   return {'x':e.clientX, 'y':e.clientY}
@@ -26,13 +25,14 @@ document.addEventListener('mousemove', function() {
   }
 })
 conBanner[0].style.cursor="move";
+
+
 var hit = document.getElementsByClassName('hit')[0];
-hit.value = "adfasdfadsf\naafadfasdfadsf\naafadfasdfadsf\naafadfasdfadsf\naafadfasdfadsf\naaf"
 var read = document.getElementsByClassName('real-font')[0];
-var font = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-var count = 0;
-while(read.scrollHeight <= read.offsetHeight) {
-  read.innerHTML = read.innerHTML + font[count];
-  count++;
-}
-read.innerHTML = read.innerHTML.slice(0, -1);
+
+
+var artice = new Artice("冰灯是流行于中国北方的一种古老的民间艺术形式。因为独特的地域优势，黑龙江可以说是制作冰灯最早的地方。传说在很早以前，每到冬季的夜晚，在松嫩平原上，人们总会看到三五成群的农夫和渔民在悠然自得地喂马和捕鱼，他们所使用的照明工具就是用冰做成的灯笼。这便是最早的冰灯。当时制作冰灯的工艺也很简单，把水放进木桶里冻成冰坨，凿出空心，放个油灯在里面，用以照明，冰罩挡住了凛冽的寒风，黑夜里便有了不灭的灯盏，冰灯成了人们生活中不可缺少的帮手。后来，每逢新春佳节和上元之夜，人们又把它加以装饰，而成为供人观赏的独特的艺术表现形式。清代《黑龙江外纪》里对此有过详细的记载：“上元，城中张灯五夜，村落妇女来观剧者，车声彻夜不绝。有镂五六尺冰为寿星灯者，中燃双炬，望之如水晶人。”其时，冰灯在南方一些地方也相继出现过。乾隆、嘉庆年间，四川诗人张问陶曾写过一首专门描写冰灯的诗，题名就叫《冰灯》，诗云：“黑夜有炎凉，冰灯吐焰长。照来消热念，凿处漏寒光。影湿星沉水，神清月里霜。三冬足文史，底用探萤囊。”南京诗人金德荣在被谪戍新疆巴里坤时，在其古风长诗《巴里坤冰灯歌》中也咏叹道：“雪山高与天山接，上有万古不化雪。朔风一夜结作冰，裁雪妙手搏为冰。以矾入冰冰不化，以烛照冰光四射。五里之内尽通明，半月能教天不夜。元夕月轮照碧空，大千人入水晶宫。”", "冰灯")
+artice.sliceStr(read);
+artice.plan(read);
+var readSpan = document.getElementsByClassName("change");
+hit.addEventListener("keyup", artice.changeColor(readSpan, hit));
